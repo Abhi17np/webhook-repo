6b70285a68ab6e81e039e7ea2980cf65f7786006
Abhishek ST <www.abhimsd17@gmail.com>
2025-07-06 01:53:23 +0530

    Fix: Bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -57,5 +57,9 @@ def events():
         })
     return jsonify(output)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render will set PORT
+    app.run(host='0.0.0.0', port=port)
+
